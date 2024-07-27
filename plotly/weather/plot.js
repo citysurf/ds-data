5fac1388d8b6c90b3acb2bba5e4b7b7f7c453418
scottwmaxwell <scottwmaxwell@protonmail.com>
2024-07-27 12:50:39 -0700

    modified colors of plot

--- plotly/weather/plot.js
+++ plotly/weather/plot.js
@@ -4,7 +4,7 @@ var trace1 = {
     type: 'scatter',
     name: "Prescott, AZ",
     marker: {
-      color: 'rgb(94, 88, 27)',
+      color: 'rgb(120, 120, 120)',
       size: 4
     }
   };
@@ -19,8 +19,19 @@ var trace1 = {
         size: 4
       }
   };
+
+  var trace3 = {
+    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    y: [100, 103, 102, 106, 108, 112, 114, 119, 110, 103, 99, 80],
+    type: 'scatter',
+    name: "Phoenix, AZ",
+    marker: {
+        color: 'rgb(158, 110, 77)',
+        size: 4
+      }
+  }
   
-  var data = [trace1, trace2];
+  var data = [trace1, trace2, trace3];
   
   var layout = {
     title: {
@@ -30,7 +41,7 @@ var trace1 = {
             color: 'white' /* Adjust font color */
         },
         pad: {
-            t: 20 /* Padding from the top */
+            t: 0 /* Padding from the top */
         }
     },
     plot_bgcolor: '#181b1f',
